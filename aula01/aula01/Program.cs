3a46fefc0479bf67b5addd496c0122c50139bf19
LeonardoDecco <leonardo720xd@hotmail.com>
2025-02-25 22:10:47 -0300

    master

---
+++ aula01/aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello, World!");
+
+string firstname = "Leonardo";
+string surname = "Decco";
+string lastName = "Santos";
+
+Console.WriteLine($"Meu nome completo é {firstname} {surname} {lastName}");
+
+int idade = 27;
+
+Console.WriteLine($"E minha idade é {idade}");
+
+Console.WriteLine($"Escreva seu nome: ");
+
+string nome = Console.ReadLine();
+
---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, qual operação matematica deseja fazer ?");
+        Console.WriteLine("1 - Adição");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplcação");
+        Console.WriteLine("4 - Divisão \n");
+
+        Console.ReadLine();
+
+        int operacao = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o primeiro numero: ")
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo numero: ")
+        int num2 = int.Parse(Console.ReadLine());
+    }
+}
